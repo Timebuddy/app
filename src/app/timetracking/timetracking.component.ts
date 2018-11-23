634c882ec98c16b2dcb2e6e04a22f295e143621d
Kim Eirik Kvassheim <norkek@gmail.com>
2018-11-24 00:45:29 +0100

    Legg til ny component; timetracking - WIP

---
+++ src/app/timetracking/timetracking.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-timetracking',
+  templateUrl: './timetracking.component.html',
+  styleUrls: ['./timetracking.component.css']
+})
+export class TimetrackingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
